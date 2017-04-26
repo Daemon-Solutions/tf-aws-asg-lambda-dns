9ca7dd51d1ce51a96a40ce5741179152d483079b
Pawel Fiuto <pawel@bashton.com>
2017-04-26 15:03:50 +0200

    Added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "lambda_manage_dns_role_arn" {
   value = "${aws_iam_role.lambda_manage_dns_role.name}"
 }
+
+output "lambda_function_arn" {
+  value = "${aws_lambda_function.manage_dns.arn}"
+}
+
+output "s3_bucket_name" {
+  value = "${aws_s3_bucket.bucket_for_lambda_package.id}"
+}
+
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.bucket_for_lambda_package.arn}"
+}
+
+output "sns_topic_arn" {
+  value = "${aws_sns_topic.manage_dns_asg_sns.arn}"
+}
