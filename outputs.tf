b41fd08ecdced7b54c648c0550d8a479acac349f
Pawel Fiuto <pawel@bashton.com>
2017-06-01 08:50:32 +0200

    Removed old outputs

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,6 @@ output "lambda_function_arn" {
   value = "${aws_lambda_function.manage_dns.arn}"
 }
 
-output "s3_bucket_name" {
-  value = "${aws_s3_bucket.bucket_for_lambda_package.id}"
-}
-
-output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.bucket_for_lambda_package.arn}"
-}
-
 output "sns_topic_arn" {
   value = "${aws_sns_topic.manage_dns_asg_sns.arn}"
 }
