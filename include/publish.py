42b8dd88a570ba5fabb5db43494f62ad7a35b693
Pawel Fiuto <pawel.fiuto@claranet.uk>
2017-11-27 14:54:03 +0100

    Notify SNS topics
    It may happen that when lambda is created along with ASG then records
    won't get created for first instance.

---
+++ include/publish.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import boto3
+import json
+import sys
+
+aws_region = sys.argv[1]
+asg_name = sys.argv[2]
+sns_topic_arn = sys.argv[3]
+
+
+client = boto3.client('sns',region_name=aws_region)
+
+message = json.dumps({
+    'Event': 'autoscaling:TEST_NOTIFICATION',
+    'AutoScalingGroupName': asg_name})
+
+response = client.publish(
+    TopicArn=sns_topic_arn,
+    Message=message)
+
+json.dump(response, sys.stdout)
